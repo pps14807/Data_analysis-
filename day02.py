1e1e685abca2dd92aea0e803c2de29060f6196b2
pps14807 <chuzhouzhi@outlook.com>
2020-03-21 23:39:21 +0800

    day02：matplotlib折线图中文刻度学习

--- day02.py
+++ day02.py
@@ -1,5 +1,10 @@
 from matplotlib import  pyplot
 import random
+import matplotlib
+from matplotlib import  font_manager
+
+my_font = font_manager.FontProperties(fname = "/System/Library/Fonts/PingFang.ttc")
+
 
 x = range(0,120)
 y = [random.randint(20,35) for i in range(120)]
@@ -7,10 +12,15 @@ y = [random.randint(20,35) for i in range(120)]
 pyplot.figure(figsize=(18,9),dpi=80)
 
 #调整x轴的刻度
-_x = x
-_xtick_labels =["hello,{}".format(i) for i in _x]
+_x = list(x)
+_xtick_labels =["10点{}分".format(i) for i in  range(0,60)]
+_xtick_labels += ["11点{}分".format(i) for i in range(0,60)]
 
-pyplot.xticks(x,_xtick_labels)  #刻度显示字符串时，必须要数字和字符串一一对应
+pyplot.xticks(_x[::10],_xtick_labels[::10],rotation = 90,fontproperties=my_font)  #刻度显示字符串时，必须要数字和字符串一一对应
+
+pyplot.xlabel("时间",fontproperties = my_font)
+pyplot.ylabel("温度",fontproperties = my_font)
+pyplot.title("10点到12点每分钟的时间变化情况",fontproperties = my_font)
 
 pyplot.plot(x,y)
 
